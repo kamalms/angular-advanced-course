9735173a7b50eb2ed602e69b9ae03b1cd4946108
kamalms <mskamalsmart@gmail.com>
2022-05-26 09:33:49 +0530

    learn ng content

--- au-tab-panel/src/app/tab/tab.component.ts
+++ au-tab-panel/src/app/tab/tab.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'tab',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab.component.scss']
 })
 export class TabComponent implements OnInit {
-
+  @Input() title : string;
+  @Input() selected :boolean;
   constructor() { }
 
   ngOnInit() {
--- au-tab-panel/src/app/tabs/tabs.component.ts
+++ au-tab-panel/src/app/tabs/tabs.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { TabComponent } from './../tab/tab.component';
+import { Component, ContentChildren, Input, OnInit, QueryList, TemplateRef } from '@angular/core';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'tabs',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsComponent implements OnInit {
 
+  @ContentChildren(TabComponent) tabs : QueryList<TabComponent>
+  @Input() tabstemplate : TemplateRef<any>;
   constructor() { }
 
   ngOnInit() {
+  }
+  ngAfterContentInit() {
+    console.log('content', this.tabs);
+    let seleted = this.tabs.find(tab => tab.selected)
+    if(!seleted) {
+      this.tabs.first.selected = true;
+    }
+  }
+
+  tabSelection(tab : TabComponent) {
+    this.tabs.forEach(tab => tab.selected = false);
+    tab.selected = true;
+  }
+
+  get tabsContext() {
+    return  {
+      tabs : this.tabs
+    }
   }
 
 }
